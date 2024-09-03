450962743db7008889e9ed21c607913e40c36cfb
kappaexpress <46478065+kappaexpress@users.noreply.github.com>
2024-09-03 11:32:33 +0000

    chore: Enable web search tool in artifacts-and-tools.ts

--- bin/artifacts-and-tools.ts
+++ bin/artifacts-and-tools.ts
@@ -6,8 +6,8 @@ import { ArtifactsAndToolsStack } from "../lib/stack";
 
 const config: StackConfig = {
   bedrockRegion: "us-east-1",
-  bedrockModel: "anthropic.claude-3-sonnet-20240229-v1:0",
-  //bedrockModel: "anthropic.claude-3-5-sonnet-20240620-v1:0",
+  //bedrockModel: "anthropic.claude-3-sonnet-20240229-v1:0",
+  bedrockModel: "anthropic.claude-3-5-sonnet-20240620-v1:0",
   playground: {
     enabled: true,
   },
@@ -18,7 +18,7 @@ const config: StackConfig = {
     enabled: true,
   },
   webSearchTool: {
-    enabled: false,
+    enabled: true,
   },
 };
 
