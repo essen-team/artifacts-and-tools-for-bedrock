a08628fc02163849c9335efb3af39f55d03fb5d7
kappaexpress <46478065+kappaexpress@users.noreply.github.com>
2024-10-27 02:56:42 +0000

    update

--- bin/artifacts-and-tools.ts
+++ bin/artifacts-and-tools.ts
@@ -5,9 +5,9 @@ import { StackConfig } from "../lib/types";
 import { ArtifactsAndToolsStack } from "../lib/stack";
 
 const config: StackConfig = {
-  bedrockRegion: "us-east-1",
+  bedrockRegion: "us-west-2",
   //bedrockModel: "anthropic.claude-3-sonnet-20240229-v1:0",
-  bedrockModel: "anthropic.claude-3-5-sonnet-20240620-v1:0",
+  bedrockModel: "anthropic.claude-3-5-sonnet-20241022-v2:0",
   playground: {
     enabled: true,
   },
